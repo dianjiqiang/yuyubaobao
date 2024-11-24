8f858d7588be235744d6c6b9c8fcc3816c5c69e4
dianjiqiang <609661157@qq.com>
2024-11-24 16:19:01 +0800

    feat: 新增模块

---
+++ core/core/bin/index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+const utils = require('@yuyubaobao/utils')
+
+
+function core() {
+    // TODO
+    console.log(utils());
+    // console.log('-------');
+    
+}
+
+
+core()
+
+
+module.exports = core;
---
+++ utils/utils/lib/utils.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+
+function utils() {
+    // TODO
+    console.log('utils');
+}
+
+
+module.exports = utils;
